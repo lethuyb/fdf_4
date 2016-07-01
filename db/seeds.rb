15dea57fc79ac44c20a2b1bef854659e3119fd47
lethuyb <lethuy.mta@gmail.com>
2016-07-01 08:18:33 +0700

    admin can show all user

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,5 @@
-Categories.create categoryName: "Food"
-Categories.create categoryName: "Drink"
+Categories.create category_name: "Food"
+Categories.create category_name: "Drink"
 20.times do |n|
   Product.create! price:  (n+1),
     product_name: "Che Tuyet#{n+1}",
@@ -8,4 +8,12 @@ Categories.create categoryName: "Drink"
     images: "/assets/dr9.jpeg",
     rating: "5",
     categories_id: 1
+end
+
+15.times do |u|
+  User.create! name: "User" + (u+1).to_s,
+  address: "nghe an",
+  phone: "242431",
+  email: "user#{u+1}@gmail.com",
+  password: "123456"
 end
